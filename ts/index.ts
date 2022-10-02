45abbdafff908c7277f91fe4ecf57f546cdc71ed
KhoaPC <luongvankhoapc@gmail.com>
2022-10-02 19:56:21 +0700

    22Oct02 TS config

---
+++ ts/index.ts
@@ -0,0 +1,14 @@
+console.log('Hello');
+let Hello = 'hello';
+
+// sourceMap: view ts file in browser
+// outDir: specify folder to save file
+// removeComments
+// noEmitOnError: no create file if error
+
+// type checking
+// strictNullChecks
+
+
+// ????
+// strictBindCallApply
