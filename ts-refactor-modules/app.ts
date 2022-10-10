95e307a3ee8d37caa3025b04d2a38f8a4f566fb3
KhoaPC <luongvankhoapc@gmail.com>
2022-10-10 09:59:57 +0700

    22Sep10 Using Webpack with TypeScript

--- ts-refactor-modules/app.ts
+++ ts-refactor-modules/app.ts
@@ -1,5 +1,5 @@
-import { ProjectInput } from './components/project-input.js';
-import { ProjectList } from './components/project-list.js';
+import { ProjectInput } from './components/project-input';
+import { ProjectList } from './components/project-list';
 
 const c = console.log;
 
@@ -7,4 +7,3 @@ new ProjectInput();
 new ProjectList('active');
 new ProjectList('finished');
 
-// we'll briefly show how each `kind of` library is used, how it is written, and list some example libraries from the real world
