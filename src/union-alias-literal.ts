db2f07780991cd9cee46499686e5351bcfd1d560
KhoaPC <luongvankhoapc@gmail.com>
2022-10-17 08:02:10 +0700

    22Oct17 UPDATE `union-alias-literal`

--- src/union-alias-literal.ts
+++ src/union-alias-literal.ts
@@ -1,13 +1,28 @@
 const c = console.log;
 
-type combinable = number | string; //type aliases | custom type
-type conversionDescriptor = 'as-number' | 'as-string';  //type aliases | custom type 
-                                    // literal : 
+// Union: khai báo một biến hoặc tham số có nhiều kiểu dữ liệu
+let id: string | number;
+id = '43526435';
+id = 43526435;
+// id = true; // Error
+
+// Literal: Định nghĩa một `type` giới hạn các giá trị được sử dụng
+type Direction = "east" | "west " | "south" | "north";
+let myDirection: Direction = "east";
+// let yourDirection: Direction = "foo"; // Error
+
+type conversionDescriptor = 'as-number' | 'as-string';
+
+// Aliases 
+type combinable = number | string; 
+
+// Sử dụng Union, Literal, Aliases
 function combine(
-    input1: number | string, // union: declare multiple data types
-    input2: combinable,
-    conversionType: conversionDescriptor
+    input1: number | string, // Union
+    input2: combinable, // Aliases
+    conversionType: conversionDescriptor // Literal
 ) {
+    // Kiểm tra các điều kiện để nối chuổi hoặc cộng
     let result;
     if (typeof input1 === 'number' && typeof input2 === "number" && conversionType === 'as-number')
         result = +input1 + +input2;
@@ -22,5 +37,5 @@ function combine(
     return result;
 }
 
-c(combine(15, 15, 'as-number'));
-c(combine('Kh', 'oa', 'as-string'));
+c(combine(15, 15, 'as-number')); // 30
+c(combine('Kh', 'oa', 'as-string')); // Khoa
